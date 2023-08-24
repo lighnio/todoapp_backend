763e4617ebead39f8cc2d42a4d4f16957e390197
lighnio <charlesvas16@outlook.com>
2023-08-24 15:00:55 -0600

    ADD: Endpoints

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user');
+const taskController = require('../controllers/task');
+
+
+router.get('/users', userController.getUsers);
+router.post('/users', userController.createUser);
+router.get('/users/:id', userController.getUserById);
+
+
+router.get('/tasks', taskController.getTasks);
+router.post('/tasks', taskController.createTask);
+router.get('/tasks/:id', taskController.getTaskById);
+
+module.exports = router;
