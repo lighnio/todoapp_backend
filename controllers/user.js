d43e9ce87227f9008e6dce77c9a55a0fa323ed66
lighnio <charlesvas16@outlook.com>
2023-08-25 10:37:30 -0600

    FIX: Better responses for better performance

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,14 @@
 const User = require('../models/user');
 
 exports.getUsers = async (req, res) => {
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = 20;
+    const offset = (page - 1) * limit;
+
     try {
-        const users = await User.find();  
+        const users = await User.find().skip(offset).limit(limit);  
         res.status(200).json(users);
-        // console.log("Obteniendo usuarios...");
-        // console.log(users);
     } catch (error) {
         res.status(500).json({ message: 'Error al obtener los usuarios', error });
     }
@@ -15,8 +18,10 @@ exports.createUser = async (req, res) => {
     try {
         const newUser = new User(req.body);
         await newUser.save();  
+        console.log(newUser);
         res.status(201).json({ message: 'Usuario creado exitosamente', user: newUser });
     } catch (error) {
+        console.log(error);
         res.status(500).json({ message: 'Error al crear el usuario', error });
     }
 };
